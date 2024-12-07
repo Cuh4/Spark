1d87a3f6e17b7e706a47e59c2c1e34bbd934ec8c
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-12-07 04:08:51 +0000

    feat: add responses directory containing responses that will be sent to clients

---
+++ src/responses/__init__.py
@@ -0,0 +1,23 @@
+"""
+----------------------------------------------
+Spark: An open-source recreation of YouTube.
+https://github.com/Cuh4/Spark
+----------------------------------------------
+
+Copyright (C) 2024 Cuh4
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# // Imports
+from . import user
